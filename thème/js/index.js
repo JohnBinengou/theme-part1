d3fc5b00872e61d15fcc8462e566e0b8bead4be2
John Borian <bbinengou@gmail.com>
2020-10-15 07:28:06 +0200

    part-1-of-themes

---
+++ thème/js/index.js
@@ -0,0 +1,13 @@
+/* ------------------------------------------ HEADER ---------------------------------------------------------------*/
+$(document).ready(function () {
+    $('.navbar-light .dmenu').hover(function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+        }, function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+        });
+    });
+
+
+
+/* ---------------------------------------- CARROUSEL -------------------------------------------------------------------- */
+
